755c96437b5a38e95a4cf286c630f2f9da9c5e92
Zoe Cohen <zoecohen01@gmail.com>
2019-07-19 11:06:17 +0300

    post and get routes in python implemented

--- store.py
+++ store.py
@@ -1,7 +1,9 @@
+
 from bottle import route, run, template, static_file, get, post, delete, request
 from sys import argv
 import json
 import pymysql
+import database_sql
 
 @get("/admin")
 def admin_portal():
@@ -27,6 +29,19 @@ def stylesheets(filename):
 @get('/images/<filename:re:.*\.(jpg|png|gif|ico)>')
 def images(filename):
     return static_file(filename, root='images')
+
+
+@post("/category")
+def category_to_database():
+    requested_category = request.POST.get("name")
+    result = database_sql.insert_category(requested_category)
+    return json.dumps(result)
+
+
+@get("/categories")
+def send_categories():
+    result = database_sql.return_categories()
+    return json.dumps(result)
 
 
 run(host='0.0.0.0', port=argv[1])
